b2cae02b6f47013a6f245fd759e4820a511c6b22
aivaraskriksciunas <aiviskri@gmail.com>
2023-05-29 21:57:58 +0300

    author pagination

--- song_app/authors/urls.py
+++ song_app/authors/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from authors.views import hello_view
+from authors.views import (
+    hello_view,
+    create_author,
+    AllAuthorsListView,
+)
 
 urlpatterns = [
-    path( "hello/", hello_view )
+    path( "hello/", hello_view ),
+    path( "new/", create_author ),
+    path( "", AllAuthorsListView.as_view() )
 ]
--- song_app/authors/views.py
+++ song_app/authors/views.py
@@ -1,5 +1,9 @@
+import datetime
+
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import ListView
+
 from authors.models import Author
 
 # Create your views here.
@@ -18,3 +22,27 @@ def hello_view( request ):
             'author': author,
         }
     )
+
+"""
+Sukurti puslapį, kuriame būtų galima sukurti autorių pagal paduotus parametrus 
+(pvz /authors/new/?first_name=vardas&last_name=pavarde). 
+Galit pasirinkti kuriuos parametrus norite leisti paduoti vartotojui, 
+ir kuriuos įrašysite per kodą (hardcoded). Siųsdami užklausas sukurkite 10 autorių.
+"""
+def create_author( request ):
+    # /authors/new/?first_name=Jonas&last_name=Jonaitis
+    first_name = request.GET.get( "first_name" )
+    last_name = request.GET.get( "last_name" )
+    # Čia turėtų būti patikrinimas, ar žmogus įvedė gerus duomenis
+
+    author = Author()
+    author.first_name = first_name
+    author.last_name = last_name
+    author.birth_date = datetime.datetime.now()
+    author.save()
+
+    return HttpResponse( "Naujas autorius pridetas" )
+
+class AllAuthorsListView( ListView ):
+    model = Author
+    paginate_by = 2
